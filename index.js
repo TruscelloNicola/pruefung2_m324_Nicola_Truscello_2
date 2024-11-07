3a4d9810366c9b9717cd92821e62b75d8f7db482
Nicola Truscello <nicola_truscello@sluz.ch>
2024-11-07 09:54:04 +0100

    Task 3: Fixed Linter Error

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ const word = generate();
 const word2 = generate();
 console.log(
   `Hello!!! 
-  The ${chalk.redBright('random word')} of today is: ${chalk.bgRed(word)}. 
+  The ${chalk.redBright(word2)} of today is: ${chalk.bgRed(word)}. 
   The word length is ${sum(word.length, 0)}.`,
 );
